9f65c0a42c591bc473a05d110de9e8ddf7d398f3
Pedro Cattori <pcattori@mit.edu>
2014-05-02 19:23:36 -0400

    Reading html. Simple parsing using jquery.

--- htmlModifier/htmlMod.js
+++ htmlModifier/htmlMod.js
@@ -1,6 +1,16 @@
+/*
 //Creating Elements
 var btn = document.createElement("BUTTON")
 var t = document.createTextNode("CLICK ME");
 btn.appendChild(t);
 //Appending to DOM 
 document.body.appendChild(btn);
+*/
+
+$(document).ready(function () {
+//    console.log($(document.body).html());
+//    console.log($('html').html());
+    var $html = $(document.documentElement.outerHTML);
+    console.log($html.find("#footnote").html());
+    console.log(document.documentElement.outerHTML);
+});
